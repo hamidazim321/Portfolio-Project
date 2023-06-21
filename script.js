35128498a1e8b1c6877a5d0d67a2b9588d54c56d
Kaiserabbas <123939543+Kaiserabbas@users.noreply.github.com>
2023-06-21 17:28:15 +0500

    Import addmenu event in script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import AddMenuEvent from './Modules/Toolbar-Menu.js';
 import DesctopToolBar from './Modules/Desktop-Dom.js';
 import ScrollSpy from './Modules/Desktop-ScrollSpy.js';
+import { LoadProjects } from './Modules/Projects.js';
 
 window.addEventListener('resize', () => {
   setTimeout(DesctopToolBar(), 100);
@@ -18,4 +19,5 @@ window.addEventListener('resize', () => {
 
 window.addEventListener('load', () => {
   AddMenuEvent();
+  LoadProjects()
 });
