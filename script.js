bd9555566dcebf00da6ebc80052fcbfbb2ecaffe
Kaiserabbas <123939543+Kaiserabbas@users.noreply.github.com>
2023-06-22 18:08:29 +0500

    Import updatestorage module

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ import AddMenuEvent from './Modules/Toolbar-Menu.js';
 import DesctopToolBar from './Modules/Desktop-Dom.js';
 import ScrollSpy from './Modules/Desktop-ScrollSpy.js';
 import { LoadProjects } from './Modules/Projects.js';
+import { UpdateForm, updateStorage } from './Modules/LocalStorage.js';
 
 window.addEventListener('resize', () => {
   setTimeout(DesctopToolBar(), 100);
@@ -15,4 +16,6 @@ window.addEventListener('load', () => {
   ScrollSpy();
   LoadProjects();
   AddMenuEvent();
+  setTimeout(200,updateStorage())
+  setTimeout(200,UpdateForm())
 });
