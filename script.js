e6e60c97f8991893b41a4e0917ad41930e5b0d7a
hamidazim321 <129533107+hamidazim321@users.noreply.github.com>
2023-06-23 16:24:56 +0500

    Responsive CSS Succesful 1

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-import AddMenuEvent from './Modules/Toolbar-Menu.js';
-import DesctopToolBar from './Modules/Desktop-Dom.js';
+// import AddMenuEvent from './Modules/Toolbar-Menu.js';
+// import DesctopToolBar from './Modules/Desktop-Dom.js';
 import ScrollSpy from './Modules/Desktop-ScrollSpy.js';
 import { LoadProjects } from './Modules/Projects.js';
 import ValidateForm from './Modules/Validations.js';
 import { UpdateForm, updateStorage } from './Modules/LocalStorage.js';
 
 window.addEventListener('resize', () => {
-  setTimeout(DesctopToolBar(), 100);
+  // setTimeout(DesctopToolBar(), 100);
   ScrollSpy();
   LoadProjects();
-  AddMenuEvent();
+  // AddMenuEvent();
 });
 
 window.addEventListener('load', () => {
-  setTimeout(DesctopToolBar(), 100);
+  // setTimeout(DesctopToolBar(), 100);
   ScrollSpy();
   LoadProjects();
-  AddMenuEvent();
+  // AddMenuEvent();
   ValidateForm();
   setTimeout(200, updateStorage());
   setTimeout(200, UpdateForm());
