a6f8e15dbf7f66eaca4e8bd5d78cb08e55bbb280
hamidazim321 <129533107+hamidazim321@users.noreply.github.com>
2023-06-23 17:06:17 +0500

    Refactor Load Nav Menu On Mobile Function

--- Modules/Toolbar-Menu.js
+++ Modules/Toolbar-Menu.js
@@ -48,3 +48,25 @@
 // }
 
 // export default AddMenuEvent;
+
+function LoadMenu(){
+  if (window.innerWidth < 767){
+    let menuBtn = document.querySelector('#tool-button')
+    let navMenu = document.querySelector('#nav-list')
+    let MenuOptions = document.querySelector('#menu-options').querySelectorAll('li')
+
+    menuBtn.addEventListener('click', ()=> {
+      navMenu = document.querySelector('#nav-list')
+      navMenu.classList.remove('hideOnMobile')
+    })
+
+    MenuOptions.forEach(li => {
+      li.addEventListener('click', ()=>{
+        navMenu.classList.add('hideOnMobile')
+      })
+    });
+  }
+
+}
+
+export default LoadMenu
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// import AddMenuEvent from './Modules/Toolbar-Menu.js';
+import LoadMenu from './Modules/Toolbar-Menu.js';
 // import DesctopToolBar from './Modules/Desktop-Dom.js';
 import ScrollSpy from './Modules/Desktop-ScrollSpy.js';
 import { LoadProjects } from './Modules/Projects.js';
@@ -16,7 +16,7 @@ window.addEventListener('load', () => {
   // setTimeout(DesctopToolBar(), 100);
   ScrollSpy();
   LoadProjects();
-  // AddMenuEvent();
+  LoadMenu()
   ValidateForm();
   setTimeout(200, updateStorage());
   setTimeout(200, UpdateForm());
